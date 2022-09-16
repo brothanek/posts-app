86504140f1c53f507b5365bb82c283c24bfd39a7
Ondrej Brothanek <brothanek.ondrej@gmail.com>
2022-09-15 21:21:54 -0400

    Add simple jest tests, setup config

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,13 @@
+// Optional: configure or set up a testing framework before each test.
+// If you delete this file, remove `setupFilesAfterEnv` from `jest.config.js`
+
+// Used for __tests__/testing-library.js
+// Learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect'
+
+import { TextEncoder, TextDecoder } from 'util'
+global.TextEncoder = TextEncoder
+global.TextDecoder = TextDecoder
+
+import dotenv from 'dotenv'
+dotenv.config({ path: '.env.local' })
